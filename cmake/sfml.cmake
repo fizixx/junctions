ab89d8835700fb7783791a02e182bfcac80d07c9
Tiaan Louw <tiaanl@gmail.com>
2015-05-12 20:23:48 +0200

    Fixed building exmaple.

--- cmake/sfml.cmake
+++ cmake/sfml.cmake
@@ -4,6 +4,10 @@ if(NOT SFML_ROOT_DIR)
   message(FATAL_ERROR "SFML not found. Please set SFML_ROOT_DIR.")
 endif()
 
+if(NOT BUILD_SHARED_LIBS)
+  add_definitions("-DSFML_STATIC")
+endif()
+
 add_subdirectory("${SFML_ROOT_DIR}" "sfml")
 
 target_include_directories("sfml-graphics" PUBLIC "${SFML_SOURCE_DIR}/include")
